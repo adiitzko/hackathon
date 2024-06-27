76cf1ec3c37d316bacfe34c649f0c249da83e49e
gilishechter <gishecht@g.jct.ac.il>
2024-06-27 16:01:51 +0300

    change api

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI,Request
+from fastapi import FastAPI,Body,Request, HTTPException,status,Form
 from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from dotenv import load_dotenv
@@ -32,16 +32,17 @@ app.add_middleware(
 )
 
 # אפשר להוסיף middleware כדי לאפשר גישה ל-Frontend מה-Backend
-@app.middleware("http")
-async def add_cors_headers(request: Request, call_next):
-    response = await call_next(request)
-    response.headers["Access-Control-Allow-Origin"] = frontend_url
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    return response
+# @app.middleware("http")
+# async def add_cors_headers(request: Request, call_next):
+#     response = await call_next(request)
+#     response.headers["Access-Control-Allow-Origin"] = frontend_url
+#     response.headers["Access-Control-Allow-Headers"] = "*"
+#     return response
 
 # Define MongoDB connection setup function
 def connect_to_mongo():
-    mongo_uri = os.getenv("MONGO_URI")
+    mongo_uri = r"mongodb+srv://adiitzko:adiitz2004@cluster0.is6jut3.mongodb.net/"
+    #mongo_uri = os.getenv("MONGO_URI") 
     if not mongo_uri:
         raise ValueError("No MONGO_URI set for MongoDB connection")
     app.mongodb_client  = MongoClient(mongo_uri)
@@ -64,12 +65,24 @@ atexit.register(close_mongo_connection)
 # Include router for location API
 app.include_router(router, tags=["locations", "users"], prefix="/api/v1")
 
+@app.post("/users/login", response_description="Check user credentials")
+def check_user_credentials(request: Request, username: str = Form(...), password: str = Form(...)):
+    user = request.app.database["users"].find_one({"username": username})
+    if user is not None:
+        return {"status": "success", "user_id": str(user["_id"])}
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid username or password"
+        )
+
 @app.get("/")
 def read_root():
-    return {"message": "Welcome to the Hackaton"}
-
+    return {"message": "Welcome to the Hackaton1"}
+        
 
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+n                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
