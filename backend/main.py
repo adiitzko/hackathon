7fd20c548b935c999c65e4492f29462bd516003e
gilishechter <gishecht@g.jct.ac.il>
2024-07-01 21:29:20 +0300

    func

--- backend/main.py
+++ backend/main.py
@@ -345,8 +345,9 @@ class Message(BaseModel):
 @app.post("/create_message/")
 def create_message(messages: Message):
     try:
-        print(messages)
-        encrypted_messaged = encrypt_message(messages.content, secret_key)
+
+        key = b'Sixteen byte key' 
+        encrypted_messaged = encrypt_message(messages.content, key)
         print(encrypted_messaged)
         message_dict = messages.dict()
         print(encrypted_messaged)
