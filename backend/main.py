cb1987c1e8a8adf9ba0917931da730c101954d30
gilishechter <gishecht@g.jct.ac.il>
2024-07-01 11:42:36 +0300

    checking

--- backend/main.py
+++ backend/main.py
@@ -172,7 +172,6 @@ def get_users():
     users = []
     cursor = users_collection.find({}, {"_id": 0, "id":1,"username": 1, "password": 1, "address": 1})  
     for user in cursor:
-        if user.get("isAdmin")=="false":
            users.append(user)
     if users!=None:
         return users
