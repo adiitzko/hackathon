bb9b2a494d170e180e9044676cf318bd312a59d3
gilishechter <gishecht@g.jct.ac.il>
2024-07-01 12:19:09 +0300

    checking

--- backend/models.py
+++ backend/models.py
@@ -4,27 +4,28 @@ from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-class UserCreate(BaseModel):
-    id: str
-    username: str
-    password: str
-    role: str
-    phone_number: str
-    address: str
-    isInDanger: bool = False  # Default value for isInDanger
-    isAdmin:bool=False
+# pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-class Message(BaseModel):
-    send: str = Field(...)
-    content: str = Field(...)
-    time: datetime = Field(default_factory=datetime.utcnow)
+# class UserCreate(BaseModel):
+#     id: str
+#     username: str
+#     password: str
+#     role: str
+#     phone_number: str
+#     address: str
+#     isInDanger: bool = False  # Default value for isInDanger
+#     isAdmin:bool=False
 
-class UserDelete(BaseModel):
-    username: str= Field(None, description="Username of the user")
-    id: str = Field(None, description="ID of the user")
+# class Message(BaseModel):
+#     send: str = Field(...)
+#     content: str = Field(...)
+#     time: datetime = Field(default_factory=datetime.utcnow)
+
+# class UserDelete(BaseModel):
+#     username: str= Field(None, description="Username of the user")
+#     id: str = Field(None, description="ID of the user")
     
-class LoginParams(BaseModel):
-    username: str
-    password: str
+# class LoginParams(BaseModel):
+#     username: str
+#     password: str
