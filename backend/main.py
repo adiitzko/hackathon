ba75ec95af8d17faadf206ee42b5c96c4aebdeab
gilishechter <gishecht@g.jct.ac.il>
2024-07-02 20:16:14 +0300

    func

--- backend/main.py
+++ backend/main.py
@@ -30,6 +30,8 @@ from starlette.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from starlette.responses import StreamingResponse
 import io
+import schedule
+import time
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class UserCreate(BaseModel):
@@ -318,7 +320,27 @@ class Location(BaseModel):
     timestamp: datetime= datetime.now()
     isInDanger: bool = False  # Default value for isInDanger
     
-   
+
+def delete_messages():
+    try:
+        # מחיקת כל המסרים בקולקציה
+        result = database.messages.delete_many({})
+        print(f"{result.deleted_count} messages deleted successfully.")
+    except Exception as e:
+        print(f"An error occurred while deleting messages: {str(e)}")
+
+# פונקציה להפעלת המחיקה כל 24 שעות
+def schedule_delete():
+    # הגדרת תזמון פעולה - ימחק כל 24 שעות
+    schedule.every(1).minute.do(delete_messages)
+    
+    # לולאה להרצת המתזמן
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+# קריאה לפונקציה שמתזמנת את המחיקה
+schedule_delete()
   
 @app.post("/add_location/")
 def add_location(location: Location):
