457e64adf756dede3ca43aa37823b8d04e74f27b
gilishechter <gishecht@g.jct.ac.il>
2024-07-04 13:55:38 +0300

    Add logger

--- backend/main.py
+++ backend/main.py
@@ -724,7 +724,7 @@ def read_roots():
     return{""}
 
 
-if '_name_' == "_main_":
+if __name__ == "_main_":
     import uvicorn
     #user = app.database["users"].find_one({"username":"adam"}    
     meet=create_meeting_util()
