b562b486ebc7ab708960d724bf523f29624d5a8f
gilishechter <gishecht@g.jct.ac.il>
2024-07-04 13:57:27 +0300

    Add logger

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ from starlette.status import HTTP_403_FORBIDDEN
 import multiprocessing
 
 import logging
-logger = logging.getLogger(_name_)
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 stream_handler = logging.StreamHandler(sys.stdout)
 log_formatter = logging.Formatter("%(asctime)s [%(processName)s: %(process)d] [%(threadName)s: %(thread)d] [%(levelname)s] %(name)s: %(message)s")
